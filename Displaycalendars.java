04f5c0aa274471fc5e888e7baa15a3f6dfc6a637
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2016-03-14 21:08:29 +0800

    use calendar
    use calendar to define month and year and change these with api Gregorian Calendar

--- Displaycalendars.java
+++ Displaycalendars.java
@@ -1,137 +1,105 @@
 //U10416012 劉宏德
-//Listing6.12
-import java.util.Scanner;
 
 public class PrintCalendar {
+
+	//load the calendar class which defined in api
+	static Calendar calendar = new calendar();
+	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
 		
-		//prompt the user to enter year
-		System.out.println("Enter full year (e.g., 2012)");
-		int year = input.nextInt();
-		
-		//prompt the user to enter month
-		System.out.println("Enter month as a number between 1 to 12: ");
-		int month = input.nextInt();
+		//define month and year wich calendar
+		int month = calendar.get(Calendar.MONTH) + 1;
+		int year = calendar.get(Calendar.YEAR);
 		
 		//print the calendar for a month in a year
 		printMonth(year, month);
 	}
 	
-	/** Print the calendar for a month in a year */
-	public static void printMonth(int year, int month) {
+	//Print the calendar for a month in a year
+	public static void printMonth(int year, int month){
+		
+		int startDay = getStartDay();
+		int numberOfDaysInMonth = calendar.daysInMonth();
+		
 		//print the headings of the calendar
 		printMonthTitle(year, month);
 		
-		//print the body of the calendar
-		printMonthBody(year, month);
+		//Print the body of the calendar
+		printMonthBody(startDay, numberOfDaysInMonth);
 	}
 	
-	/** Print the month title, e.g., March 2012 */
-	public static void printMonthTitle(int year, int month) {
-		System.out.println("         " + getMonthName(month) + " " + year);
-		System.out.println("-----------------------------");
+	//Print the month title
+	public static void printMonthTitle(int year, int month){
+		System.out.println("        " + calendar.getMonthName() + "   " + calendar.get(Calendar.YEAR));
+		System.out.println("---------------------------");
 		System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
 	}
 	
-	/**Get the English name for the month */
-	public static String getMonthName(int month) {
+	//Print month body
+	public static void printMonthBody(int startDay, int numberOfDaysInMonth){
+		
+		//Pad space before the first day of the month
+		for(int i = 0; i < (startDay - 1); i++) {
+			System.out.print("    ");
+		}
+		for(int i = 1; i <= numberOfDaysInMonth; i++) {
+			System.out.printf("%4d", i);
+			
+			if((i + startDay - 1) % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
+	
+	// Get the start day
+	static int getStartDay(){
+		return calendar.get(Calendar.DAY_OF_WEEK);
+	}
+}
+
+	
+class Calendar extends GregorianCalendar{
+	//Get the number of days in a month
+	public int daysInMonth(){
+		
+		int month = get(MONTH) + 1;
+		
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			return 31;
+		}
+		
+		if (month == 4 || month == 6 || month == 9 || month == 11) {
+			return 30;
+		}
+		
+		if (month == 2) {
+			return isLeapYear(get(YEAR)) ? 29 : 28;
+		}	
+		
+		return 0; 
+	}
+
+	//Get the English name for the month
+	public String getMonthName() {
+		
 		String monthName = "";
-		switch (month) {
-			case 1: monthName = "January";		break;
-			case 2: monthName = "February";		break;
-			case 3: monthName = "March";		break;
-			case 4: monthName = "April";		break;
-			case 5: monthName = "May";			break;
-			case 6: monthName = "June";			break;
-			case 7: monthName = "July";			break;
-			case 8: monthName = "August";		break;
-			case 9: monthName = "September";	break;
-			case 10: monthName = "October";		break;
-			case 11: monthName = "November";	break;
+		
+		switch (get(MONTH) + 1) {
+			case 1: monthName = "January"; 		break;
+			case 2: monthName = "February"; 	break;
+			case 3: monthName = "March"; 		break;
+			case 4: monthName = "April"; 		break;
+			case 5: monthName = "May"; 			break;
+			case 6: monthName = "June"; 		break;
+			case 7: monthName = "July"; 		break;
+			case 8: monthName = "August"; 		break;
+			case 9: monthName = "September"; 	break;
+			case 10: monthName = "October"; 	break;
+			case 11: monthName = "November"; 	break;
 			case 12: monthName = "December";
 		}
 		
 		return monthName;
-	}
-	
-	/** Print month body */
-	public static void printMonthBody(int year, int month) {
-		//Get start day of the week for the first date in the month
-		int startDay = getStartDay(year, month);
-		
-		//Get number of days in the month
-		int numberOfDaysInMonth = getNumberOfDaysInMonth(year, month);
-		
-		// Pad space before the first day of the month
-		int i = 0;
-		for(i = 0; i < startDay; i++) {
-			System.out.print("    ");
-		}
-		
-		for(i = 1; i <= numberOfDaysInMonth; i++) {
-			System.out.printf("%4d", i);
-			
-			if((i + startDay) % 7 == 0) {
-				System.out.println();
-			}
-		}
-		
-		System.out.println();
-	}
-	
-	/** Get the start day of the month/1/year */
-	public static int getStartDay(int year, int month) {
-		final int START_DAY_FOR_JAN_1_1800 = 3;
-		//Get total number of days from 1/1/1800 to month/1/year
-		int totalNumberOfDays = getTotalNumberOfDays(year, month);
-		
-		//return the start day for month/1/year
-		return (totalNumberOfDays + START_DAY_FOR_JAN_1_1800) % 7;
-	}
-	
-	/** Get the total number of days since January 1, 1800 */
-	public static int getTotalNumberOfDays(int year, int month) {
-		int total = 0;
-		
-		// Get the total days from 1800 to 1/1/year
-		for(int i = 1800; i < year; i++) {
-			if(isLeapYear(i)) {
-				total = total + 366;
-			}
-			
-			else {
-				total = total + 365;
-			}
-		}
-		
-		//Add days from Jan to the month prior to the calendar month
-		for(int i = 1; i < month; i++) {
-			total = total + getNumberOfDaysInMonth(year, i);
-		}
-		
-		return total;
-	}
-	
-	/**Get the number of days in a month */
-	public static int getNumberOfDaysInMonth(int year, int month) {
-		if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-			return 31;
-		}
-		
-		if(month == 4 || month == 6 || month == 9 || month == 11) {
-			return 30;
-		}
-		
-		if(month == 2) {
-			return isLeapYear(year) ? 29 : 28;
-		}
-		
-		return 0;
-	}
-	
-	/** Determine if it is a leap year */
-	public static boolean isLeapYear(int year) {
-		return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
 	}
 }
